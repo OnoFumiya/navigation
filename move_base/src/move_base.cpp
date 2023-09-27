9631845da683223d9dcc0647b2a4336adcd41311
OnoFumiya <fumiyaono.choi@gmail.com>
2023-09-27 14:34:41 +0900

    develop

--- move_base/src/move_base.cpp
+++ move_base/src/move_base.cpp
@@ -914,7 +914,7 @@ namespace move_base {
                            cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z );
           last_valid_control_ = ros::Time::now();
           //make sure that we send the velocity command to the base
-          vel_pub_.publish(cmd_vel);
+          // vel_pub_.publish(cmd_vel);
           if(recovery_trigger_ == CONTROLLING_R)
             recovery_index_ = 0;
         }
